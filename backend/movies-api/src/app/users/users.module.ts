7daa7b51a259e02b5a395029310d2e853eb9d5f8
oishtiaq <osama.ishtiaq@oceanhealthsystems.com>
2021-11-18 18:39:08 +0500

    integrate typeorm and db

--- backend/movies-api/src/app/users/users.module.ts
+++ backend/movies-api/src/app/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Module({
   providers: [UsersService],
   exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
 })
 export class UsersModule {}
--- backend/movies-api/src/app/users/users.service.ts
+++ backend/movies-api/src/app/users/users.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { STATIC_USERS } from '@shared/common/constants';
-
-// This should be a real class/interface representing a user entity
-export type User = any;
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ENVIRONMENT, ENV_KEYS, STATIC_USERS } from '@shared/common/constants';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users = STATIC_USERS;
+  constructor(
+    @InjectRepository(User) private usersRepo: Repository<User>,
+    private config: ConfigService,
+  ) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  async findOne(username: string): Promise<User> {
+    if (this.config.get(ENV_KEYS.ENVIRONMENT) === ENVIRONMENT.DEV) {
+      return STATIC_USERS.find((x) => x.username == username);
+    }
+
+    return this.usersRepo.findOne({
+      where: {
+        username: username,
+      },
+    });
   }
 }
